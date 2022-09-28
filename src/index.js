9f2e07f9ba1dbcc8ab7720f8c94a5229ffd38a64
vivek <vivekkumarvky13@gmail.com>
2022-09-28 12:57:50 +0530

    make it better

--- src/index.js
+++ src/index.js
@@ -8,7 +8,75 @@ app.use(express.json())
 app.use(cors())
 
 
-const Product = require("./models/product.model");
+
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please Enter product Name"],
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: [true, "Please Enter product Price"],
+    maxLength: [8, "Price cannot exceed 8 characters"],
+  },
+
+  description: {
+    type: String,
+    required: [true, "Please Enter Product Description"],
+  },
+  ratings: {
+    type: Number,
+    default: 0,
+  },
+  images: {
+    type: String,
+    required: [true, "Please Enter Produc images"],
+  },
+
+  category: {
+    type: String,
+    required: [true, "Please Enter Product Category"],
+  },
+  brand: {
+    type: String,
+    required: [true, "Please Enter Product Brand"],
+  },
+  gender: {
+    type: String,
+    required: [true, "Please Enter Gender"],
+  },
+  companydetails: {
+    type: String,
+    required: [true, "Please Enter company Details"],
+  },
+
+  sizeinfo: {
+    type: String,
+    required: [true, "Please Enter size Info"],
+  },
+
+  Stock: {
+    type: Number,
+    required: [true, "Please Enter product Stock"],
+
+    default: 1,
+  },
+  numOfReviews: {
+    type: Number,
+    default: 0,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+let Product = mongoose.model("Product", productSchema);
+
 const ApiFeatures = require("./utils/apifeatures");
 
 const registerController = require('./controllers/register.controller');
