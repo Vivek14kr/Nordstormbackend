2515d1175105ecac2ec92b2759bc053d3ae3e1ef
Vivek Kumar <72031992+Vivek14kr@users.noreply.github.com>
2023-01-07 23:51:23 +0530

    Update db.js

--- src/configs/db.js
+++ src/configs/db.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose')
 require("dotenv").config();
 
 module.exports = () => {
-  return mongoose.connect(process.env.MONGO_URI);
+  return mongoose.connect(
+    "mongodb+srv://Vivek14kr:1HqOiJ5BQivV75gP@cluster0.tfkryts.mongodb.net/Nordstorm", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }).then((res)=> console.log("connected")).catch((err) => console.log("eror: ", err));
+    
+  
 }
-
-
